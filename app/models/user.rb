79e544bd77ace3848280bbab864cf31617348d03
Rikuba <rikuba@live.jp>
2021-03-04 11:39:40 +0900

    ユーザ情報に住所と自己紹介を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :introduction, length: { maximum: 400 }
 end
---
+++ db/migrate/20210304015703_add_introduction_to_user.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class AddIntroductionToUser < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :introduction, :text
+  end
+end
