f539bd029e1e398f1bb8376a20e04b07f50718cf
Rikuba <rikuba@live.jp>
2021-03-30 17:02:36 +0900

    モデルのテストを追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,11 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'editable?' do
+    report = reports(:alices_report)
+    alice = users(:alice)
+    bob = users(:bob)
+    assert report.editable?(alice)
+    assert_not report.editable?(bob)
+  end
 end
